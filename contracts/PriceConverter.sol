92f0d6d810691bbc4762dc7a4293cf6cb1b7ff1c
Chikezie <68447285+3ill@users.noreply.github.com>
2023-05-07 00:38:08 +0100

    Add files via upload
    Added the contract directory

---
+++ contracts/PriceConverter.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.12;
+
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
+library PriceConverter {
+    function getPrice(
+        AggregatorV3Interface priceFeed
+    ) internal view returns (uint256) {
+        //Address 0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e
+
+        (, int256 price, , , ) = priceFeed.latestRoundData();
+        return uint256(price * 1e10);
+    }
+
+    function getConversionRate(
+        uint256 ethAmount,
+        AggregatorV3Interface priceFeed
+    ) internal view returns (uint256) {
+        uint256 ethPrice = getPrice(priceFeed);
+        uint256 ethAmountinUSD = (ethAmount * ethPrice) / 1e18;
+        return ethAmountinUSD;
+    }
+
+    /**function getVersion() internal view returns (uint256) {
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(
+            0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e
+        );
+        return priceFeed.version();
+    }**/
+}
